0f95bb3ed6be006b0f2b8372e08df11dda2de4b1
Alex Gichau <gichaumburu@gmail.com>
2026-03-14 00:07:23 +0300

    add github pages action

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let repo = 'reception-events-booking-system';
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  const repoNameMatch = process.env.GITHUB_REPOSITORY?.match(/.*?\/(.*)/);
+  repo = repoNameMatch ? repoNameMatch[1] : repo;
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  assetPrefix,
+  basePath,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
